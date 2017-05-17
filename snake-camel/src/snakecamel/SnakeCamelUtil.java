eb09a1b3446b3b59a018409e8a62a18078c0e811
g15922my <g15922my@s20628.tsuda.ac.jp>
2017-05-17 19:34:59 +0900

    キャメルケース　スネークケース変換においてすべてのケースで変更するように変更しました。

--- snake-camel/src/snakecamel/SnakeCamelUtil.java
+++ snake-camel/src/snakecamel/SnakeCamelUtil.java
@@ -5,7 +5,7 @@ public class SnakeCamelUtil {
 	public static String snakeToCamelcase(String snake_case) {
 		String[] words = snake_case.split("_");
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length; i++) {
 			sb.append(capitalize(words[i]));
 		}
 		return new String(sb);
@@ -14,7 +14,7 @@ public class SnakeCamelUtil {
 	public static String camelToSnakecase(String camelcase) {
 		StringBuilder sb = new StringBuilder();
 		int j = 0;
-		for (int i = 0; i < camelcase.length(); i++) {
+		for (int i = 0; i <camelcase.length(); i++) {
 			char c = camelcase.charAt(i);
 			if (Character.isUpperCase(c)) {
 				sb.append(camelcase.substring(j, i));
@@ -22,25 +22,28 @@ public class SnakeCamelUtil {
 					sb.append("_");
 				}
 				sb.append(Character.toLowerCase(c));
-				j = i;
+				j = i+1;
 			}
-		}
-		sb.append(camelcase.substring(j));
+		}		
+		sb.append(camelcase.substring(j));//大文字にしたもの(j)を飛ばしてそれ以降のものを表示
 		return new String(sb);
+	}	
+	static String capitalize(String s) {//大文字にする				
+		for(int i=0;i<s.length();i++){
+			char first = s.charAt(i);//0だと1文字のときだけになる
+			char upperFirst = Character.toUpperCase(first);
+			String rest = s.substring(1);
+			return upperFirst + rest;
+		}
+		return s;
 	}
-	
-	static String capitalize(String s) {
-		char first = s.charAt(0);
-		char upperFirst = Character.toUpperCase(first);
-		String rest = s.substring(1);
-		return upperFirst + rest;
-	}
-
-	static String uncapitalize(String s) {
+	static String uncapitalize(String s) {//小文字にする
+		for(int i=0;i<s.length();i++){
 		char first = s.charAt(0);
 		char lowerFirst = Character.toLowerCase(first);
 		String rest = s.substring(1);
 		return lowerFirst + rest;
-	}
-	
+		}
+		return s;
+	}	
 }
